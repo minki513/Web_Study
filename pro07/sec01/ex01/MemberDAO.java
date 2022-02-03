25f2bdb800b79b5b671f3c122ddbaaecfc6ae370
minki513 <89339498+minki513@users.noreply.github.com>
2022-02-03 10:53:37 +0900

    MemberDAO 클래스
    회원 정보 조회 sql문을 실행하여 조회한 레코드들의 컬럼값을 다시 MemberVO 객체의 속성에 설정한 다음 ArrayList에 저장하고 호출 된 곳으로 반환

---
+++ pro07/sec01/ex01/MemberDAO.java
@@ -0,0 +1,63 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+public class MemberDAO {
+	private static final String driver = "com.mysql.cj.jdbc.Driver";
+	private static final String url = "jdbc:mysql://localhost:3306/pro07";
+	private static final String user = "root";
+	private static final String pwd = "rlaalsrud11@";
+	private Connection con;
+	private Statement stmt;
+	
+	public List<MemberVO> listMembers(){
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			connDB(); //네 가지 정보로 데이터베이스 연결
+			String query = "select * from t_member ";
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query); //결과값 담기
+			
+			while(rs.next()) { //조회한 레코드의 각 컬럼 값을 받아옴
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO(); //각 컬럼 값을 다시 membervo()객체의 속성에 설정
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo); //설절된 MemberVO() 객체를 다시 arrayList에저장
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("MySQL 드라이버 로딩 성공");
+			
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+			
+			stmt = con.createStatement();
+			System.out.println("Statement 생성 성공");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
