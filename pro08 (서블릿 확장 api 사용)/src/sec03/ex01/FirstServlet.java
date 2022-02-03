5565d3994ccdaf67e233d597c705ea385584881b
minki513 <89339498+minki513@users.noreply.github.com>
2022-02-03 11:11:07 +0900

    dispatch를 이용한 포워딩
    dispatch 방식은 클라이언트의 브라우저를 거치지 않고 서버에서 포워딩 진행

---
+++ pro08 (서블릿 확장 api 사용)/src/sec03/ex01/FirstServlet.java
@@ -0,0 +1,23 @@
+package sec03.ex01;
+//dispatch 방식은 클라이언트의 브라우저를 거치지않고 서버에서 포워딩이 진행
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		RequestDispatcher dispatch = request.getRequestDispatcher("second");
+		dispatch.forward(request, response);
+	}
+
+}
