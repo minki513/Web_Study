9beb06c1c68f8c01a5a5f235721bb9d367b765f9
minki513 <89339498+minki513@users.noreply.github.com>
2022-02-14 15:03:36 +0900

    SimpleUrlController 이용해 스프링 MVC 실습
    매핑된 요청에 대해 컨트롤러의 기능을 수행함

--- pro21 (스프링 MVC 기능)/src/spring/ex01/SimpleUrlController.java
+++ pro21 (스프링 MVC 기능)/src/spring/ex01/SimpleUrlController.java
@@ -1,1 +1,17 @@
+package com.spring.ex01;
 
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class SimpleUrlController implements Controller { //스프링에서 제공하는 controller인터페이스를 반드시 구현
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return new ModelAndView("index.jsp"); //작업을 마친 후 뷰이름을 modelandview에 index.jsp로 설정하여 반환
+	}
+}
