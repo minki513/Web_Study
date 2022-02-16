144c1e49393e342d27cda3dae9cc89422d585731
minki513 <89339498+minki513@users.noreply.github.com>
2022-02-16 19:53:11 +0900

    컨트롤러 클래스

---
+++ pro25 (스프링 트랜잭션)/src/com/spring/account/AccountController.java
@@ -0,0 +1,23 @@
+package com.spring.account;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class AccountController extends MultiActionController {
+	public AccountService accService;
+	
+	public void setAccService(AccountService accService) {
+		this.accService = accService;
+	}
+	
+	public ModelAndView sendMoney(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		accService.sendMoney(); //금액을 이체
+		mav.setViewName("result");
+		return mav;
+	}
+	
+}
