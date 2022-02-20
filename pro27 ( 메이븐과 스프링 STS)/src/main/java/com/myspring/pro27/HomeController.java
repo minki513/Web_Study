e783d76fece2c697f2d32c6935249f1d41aae419
minki513 <89339498+minki513@users.noreply.github.com>
2022-02-20 17:46:17 +0900

    STS 프로젝트 실행

--- pro27 ( 메이븐과 스프링 STS)/src/main/java/com/myspring/pro27/HomeController.java
+++ pro27 ( 메이븐과 스프링 STS)/src/main/java/com/myspring/pro27/HomeController.java
@@ -1,1 +1,37 @@
+package com.myspring.pro27;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "main";
+	}
+	/*
+	@RequestMapping(value = "/", method = RequestMethod.GET)  //모든 요청에 대해 home()메서드를 호출함
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String formattedDate = dateFormat.format(date);
+		model.addAttribute("serverTime", formattedDate );  //브라우저에서 요청한 시각을 jsp로 전달
+		
+		return "home"; //jsp이름을 반환
+	}*/
+	
+}
