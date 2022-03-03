fdbee993d8e4933688f5af0cdcc077490af5ef24
minki513 <89339498+minki513@users.noreply.github.com>
2022-03-03 10:49:57 +0900

    상품관련정보 service

---
+++ bookShop01/src/main/java/com/bookshop01/goods/service/GoodsServiceImpl.java
@@ -0,0 +1,54 @@
+package com.bookshop01.goods.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bookshop01.goods.dao.GoodsDAO;
+import com.bookshop01.goods.vo.GoodsVO;
+import com.bookshop01.goods.vo.ImageFileVO;
+
+@Service("goodsService")
+@Transactional(propagation=Propagation.REQUIRED)
+public class GoodsServiceImpl implements GoodsService{
+	@Autowired
+	private GoodsDAO goodsDAO;
+	
+	public Map<String,List<GoodsVO>> listGoods() throws Exception { //베스트셀러,신간, 스테디셀러를 sql문의 조건으로 전달하여 도서 정보 조회해서 hashMap에 저장
+		Map<String,List<GoodsVO>> goodsMap=new HashMap<String,List<GoodsVO>>();
+		List<GoodsVO> goodsList=goodsDAO.selectGoodsList("bestseller");
+		goodsMap.put("bestseller",goodsList);
+		goodsList=goodsDAO.selectGoodsList("newbook");
+		goodsMap.put("newbook",goodsList);
+		
+		goodsList=goodsDAO.selectGoodsList("steadyseller");
+		goodsMap.put("steadyseller",goodsList);
+		return goodsMap;
+	}
+	
+	public Map goodsDetail(String _goods_id) throws Exception {
+		Map goodsMap=new HashMap();
+		GoodsVO goodsVO = goodsDAO.selectGoodsDetail(_goods_id);
+		goodsMap.put("goodsVO", goodsVO);
+		List<ImageFileVO> imageList =goodsDAO.selectGoodsDetailImage(_goods_id);
+		goodsMap.put("imageList", imageList);
+		return goodsMap; //상품 정보와 이미지 정보를 조회한 후 HashMap에 저장
+	}
+	
+	public List<String> keywordSearch(String keyword) throws Exception {
+		List<String> list=goodsDAO.selectKeywordSearch(keyword);
+		return list;
+	}
+	
+	public List<GoodsVO> searchGoods(String searchWord) throws Exception{
+		List goodsList=goodsDAO.selectGoodsBySearchWord(searchWord);
+		return goodsList;
+	}
+	
+	
+}
