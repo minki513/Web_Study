fdf3a617b1b5aa13fd70b3a5e22f942f3898bdac
minki513 <89339498+minki513@users.noreply.github.com>
2022-03-03 10:35:28 +0900

    메인 페이지 컨트롤러

--- bookShop01/src/main/java/com/bookshop01/main/MainController.java
+++ bookShop01/src/main/java/com/bookshop01/main/MainController.java
@@ -1,1 +1,40 @@
+package com.bookshop01.main;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookshop01.common.base.BaseController;
+import com.bookshop01.goods.service.GoodsService;
+import com.bookshop01.goods.vo.GoodsVO;
+
+@Controller("mainController")
+@EnableAspectJAutoProxy
+public class MainController extends BaseController {
+	@Autowired
+	private GoodsService goodsService;
+
+	@RequestMapping(value= "/main/main.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView main(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		
+		session=request.getSession();
+		session.setAttribute("side_menu", "user"); //side_menu의 값에 따라 화면 왼쪽에 표시되는 메뉴 항목을 다르게 함
+		Map<String,List<GoodsVO>> goodsMap=goodsService.listGoods();
+		mav.addObject("goodsMap", goodsMap); //메인 페이지로 상품 정보를 전달
+		return mav;
+	}
+}
