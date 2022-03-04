dd336e69ab0028a20a804b990766bd7768bbd839
minki513 <89339498+minki513@users.noreply.github.com>
2022-03-04 20:04:35 +0900

    주문기능 service

---
+++ bookShop01/src/main/java/com/bookshop01/order/service/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+package com.bookshop01.order.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
+import com.bookshop01.order.dao.OrderDAO;
+import com.bookshop01.order.vo.OrderVO;
+
+
+@Service("orderService")
+@Transactional(propagation=Propagation.REQUIRED)
+public class OrderServiceImpl implements OrderService {
+	@Autowired
+	private OrderDAO orderDAO;
+	
+	public List<OrderVO> listMyOrderGoods(OrderVO orderVO) throws Exception{
+		List<OrderVO> orderGoodsList;
+		orderGoodsList=orderDAO.listMyOrderGoods(orderVO);
+		return orderGoodsList;
+	}
+	
+	public void addNewOrder(List<OrderVO> myOrderList) throws Exception{
+		orderDAO.insertNewOrder(myOrderList); //주문상품 목록을 테이블에 추가
+		orderDAO.removeGoodsFromCart(myOrderList); //장바구니에서 주문한 경우 해당 상품을 장바구니에서 삭제
+	}	
+	
+	public OrderVO findMyOrder(String order_id) throws Exception{
+		return orderDAO.findMyOrder(order_id);
+	}
+
+}
